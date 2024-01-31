cc0efdcef34b885d014644f040c142e5468c0f74
JerrySabino <jer.lsabino@gmail.com>
2024-01-31 00:51:00 -0400

    New Parallax Banner in About section

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { ParallaxBanner } from 'react-scroll-parallax';
+import { ParallaxProvider } from "react-scroll-parallax"
 import { motion } from 'framer-motion';
 import styles from "./About.module.css"
 
@@ -17,23 +19,24 @@ export const About = () => {
           opacity: 1,
           transition: {
             type: "spring",
-            bounce: 0.4,
+            bounce: 0.6,
             duration: 1.5
           }
         }
     };
     
     return (
-        <motion.div
+        <motion.div id="about"
             variants={onViewVariants}
             initial="offscreen"
             whileInView="onscreen"
             viewport={{once: true, amount: 0.2}}>
-            <section className={styles.container} id="about">
+            <section className={styles.container}>
                 <div className={styles.title}>
                     my values
                 </div>
                 <div className={styles.content}>
+                    {/* VALUES */}
                     <ul className={styles.aboutItems}>
                         <li className={styles.aboutItem}>
                             <div className={styles.front}>
@@ -81,6 +84,68 @@ export const About = () => {
                         </li>
                     </ul>
                 </div>
+                {/* PARALLAX BANNER */}
+                <motion.div id="about"
+                    variants={onViewVariants}
+                    initial="offscreen"
+                    whileInView="onscreen"
+                    viewport={{once: true, amount: 0.25}}>
+                    <div className={styles.box}>
+                        <div className={styles.banner_container}>
+                            <ParallaxProvider>
+                                <ParallaxBanner
+                                    layers={[
+                                        /* Sky, Stars, Moon */
+                                        { image: '../../../assets/about/layers/sky.webp',
+                                            speed: 8},
+                                        { image: '../../../assets/about/layers/stars.webp',
+                                            translateY: [1, 25]},
+                                        { image: '../../../assets/about/layers/moon.webp',
+                                            translateY: [1, 18]},
+                                        /* Mountains */
+                                        { image: '../../../assets/about/layers/mountain1.webp',
+                                            translateY: [-5, 4],
+                                            speed: 2},
+                                        { image: '../../../assets/about/layers/mountain2.webp',
+                                            translateY: [-5, 1],
+                                            speed: 2},
+                                        { image: '../../../assets/about/layers/mountain3.webp',
+                                            translateY: [-1.5],
+                                            speed: 0.3},
+                                        /* Trees */
+                                        { image: '../../../assets/about/layers/tree1.webp',
+                                            translateY: [2, -1],
+                                            speed: 0.5},
+                                        { image: '../../../assets/about/layers/reflection.webp',
+                                            translateY: [2, 0]},
+                                        { image: '../../../assets/about/layers/tree2.webp',
+                                            translateY: [-1.5],
+                                            speed: 0.5},
+                                        { image: '../../../assets/about/layers/tree3.webp',
+                                            translateY: [2, -2],
+                                            speed: 3}, 
+                                        { image: '../../../assets/about/layers/tree4.webp',
+                                            translateY: [1.8, -1],
+                                            speed: 1.5},
+                                        /* Waterline */
+                                        { image: '../../../assets/about/layers/waterline.webp',
+                                            translateY: [51, 46]},
+                                        /* Boat */
+                                        { image: '../../../assets/about/layers/boat.webp',
+                                            translateY: [5, -10]},
+                                        /* Grass */
+                                        { image: '../../../assets/about/layers/grass1.png',
+                                            translateY: [18, -8]},
+                                        { image: '../../../assets/about/layers/grass2.png',
+                                            translateY: [14, -9.5]},
+                                    ]}
+                                    className={styles.banner}
+                                    >
+                                </ParallaxBanner>
+                            </ParallaxProvider>
+                        </div>  
+                    </div>
+                </motion.div>
             </section>
         </motion.div>
     )
